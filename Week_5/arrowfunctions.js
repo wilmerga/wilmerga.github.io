5b498c985ec23c42d8216386e8e035a532e143f5
wilmerga <wilmerdavid.garcia@outlook.com>
2022-02-08 19:39:48 +0100

    Week5

---
+++ Week_5/arrowfunctions.js
@@ -0,0 +1,12 @@
+let addFive = (num) => {
+  num + 5;
+};
+
+let divideBy = (x, y) => {
+  return x / y;
+};
+
+let logToConsole = () => {
+  let msg = "MIT Coding Certificate";
+  console.log(msg);
+};
